cdc5db1d4aa35c57420bb0abdac4530322a645ac
KazChe <kaz.che@gmail.com>
2020-09-27 22:08:47 -0700

    added new intent, SendIntent.

--- app.js
+++ app.js
@@ -8,41 +8,41 @@ const twilio = require('twilio')(accountSid, authToken)
 
 module.exports.alexatext = async (event, context) => {
 
-    try {
-        let req = event.request
+let req = event.request
+console.log(">>>>>", req.intent.name)
+if (req.type === "LaunchRequest") {
+    let options = {}
+    options.speechText = "Welcome to text message app. Sends a text message to your phone",
+        options.repromptText = "For example you can say text me at then the phone number",
+        options.endSession = false
+    return buildResponse(options)
 
-        if(req.type === "LaunchRequest") {
-            let options = {}
-            options.speechText="Welcome to text message app. Sends a text message to your phone",
-                options.repromptText="For example you can say text me at then the phone number",
-                options.endSession=false
-            context.succeed(buildResponse(options))
+} else if (req.type === "IntentRequest") {
+    let options = {}
 
-        } else if(req.type === "IntentRequest") {
-            let options = {}
+    if (req.intent.name === "HelloIntent") {
+        console.log(req.intent.name === "HelloIntent")
+        var phone = req.intent.slots.phoneNumber.value
+        options.speechText = "Sending text message to " + phone + "."
+        options.endSession = true
+        await twilio.messages
+            .create({body: phone, from: tphone, to: mPhone})
+            .then(message => console.log(message.sid))
+        return buildResponse(options)
 
-            if(req.intent.name === "HelloIntent") {
-                var phone = req.intent.slots.phoneNumber.value
-                options.speechText = "Sending text message to "+phone+"."
-                options.endSession = true
-                await twilio.messages
-                    .create({body: 'Hi there!', from: tphone, to: mPhone})
-                    .then(message => console.log(message.sid))
-                return buildResponse(options)
-            } else {
-                throw "Unknown Intent"
-            }
-
-        } else if(req.type === "SessionEndedRequest") {
-
-        } else {
-            throw "Unknown Intent Type"
-        }
-
-    } catch (e) {
-        context.fail("Error::: ", e)
+    } else if (req.intent.name === "SendIntent") {
+        console.log(req.intent.name === "SendIntent")
+        var msg = req.intent.slots.messageText.value
+        options.speechText = "Sending message " + msg + "."
+        options.endSession = true
+        await twilio.messages
+            .create({body: msg, from: tphone, to: mPhone})
+            .then(message => console.log(message.sid))
+        return buildResponse(options)
     }
-};
+
+}
+}
 
 function buildResponse(options) {
     let response = {
@@ -55,7 +55,7 @@ function buildResponse(options) {
             shouldEndSession: options.endSession
         }
     }
-    if(options.repromptText) {
+    if (options.repromptText) {
         response.response.reprompt = {
             outputSpeech: {
                 type: "PlainText",
